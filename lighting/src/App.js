92bd190723ef20f4c2e4a44e1090a20a9f41e47f
deanagan <dean.agan@adslot.com>
2021-06-29 12:00:30 +1000

    Sample code around fragments

--- lighting/src/App.js
+++ lighting/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Lighting } from './Lighting';
+import TableWithFragment from './TableWithFragment';
 
 function App() {
   return (
     <div className="App">
       <h1>Lighting App</h1>
       <Lighting initial={false} />
+      <TableWithFragment />
     </div>
   );
 }
