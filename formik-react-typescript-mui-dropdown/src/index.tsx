058614c82f1cb7e47eb71ec77f1d42176863cc10
Dean Agan <agandfr@gmail.com>
2024-07-14 12:57:26 +1000

    Remove unused

--- formik-react-typescript-mui-dropdown/src/index.tsx
+++ formik-react-typescript-mui-dropdown/src/index.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
 
